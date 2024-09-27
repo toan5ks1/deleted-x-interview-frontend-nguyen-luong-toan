fbb5dcfc689c6ba9bbf833ac8093cce16cc2f25d
toan5ks1 <vipteamvnclass@gmail.com>
2024-09-27 23:08:10 +0700

    fix: remove wrong relation

--- src/db/schema/categories.ts
+++ src/db/schema/categories.ts
@@ -1,9 +1,6 @@
-import { relations } from 'drizzle-orm';
 import { pgTable, text, varchar } from 'drizzle-orm/pg-core';
 
 import { generateId } from '@/libs/id';
-
-import { meta } from './meta';
 
 export const categories = pgTable('categories', {
   id: varchar('id', { length: 30 })
@@ -12,10 +9,5 @@ export const categories = pgTable('categories', {
   name: text('name').notNull().unique(),
   slug: text('slug').notNull().unique(),
 });
-
-// Relationships
-export const categoryRelations = relations(categories, ({ many }) => ({
-  metas: many(meta), // One category has many 'meta' entries
-}));
 
 export type Category = typeof categories.$inferSelect;
