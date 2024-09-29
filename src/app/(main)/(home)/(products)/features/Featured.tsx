3a79c58651341ada75b669571f34cff1532ea89f
toan5ks1 <vipteamvnclass@gmail.com>
2024-09-30 01:24:43 +0700

    chore: clean code & hide featured cmp on mobile

--- src/app/(main)/(home)/(products)/features/Featured.tsx
+++ src/app/(main)/(home)/(products)/features/Featured.tsx
@@ -14,41 +14,40 @@ interface FeaturedProps {
   mobile?: boolean;
 }
 
-export const Featured = memo(({ items = [] }: FeaturedProps) => {
+export const FeaturedProduct = memo(({ mobile, items = [] }: FeaturedProps) => {
   return (
-    <GridShowcase
-      innerProps={{ gap: 24 }}
-      style={{ maxHeight: 'calc(100% - 104px)', maxWidth: 1024, minHeight: 162 }}
-      width={'100%'}
-    >
-      <Grid maxItemWidth={280} rows={4}>
-        {items.map((item, index) => (
-          <Card key={item.product?.identifier ?? index} showCategory {...item} />
-        ))}
-      </Grid>
-    </GridShowcase>
-  );
-});
-
-export const Figure = memo(() => {
-  return (
-    <Flexbox align="end" flex={1} height={'100%'} style={{ position: 'relative' }}>
-      <Image
-        alt={'new-arrive-figure'}
-        className="object-cover rounded-sm"
-        fill
-        loading="lazy"
-        sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
-        src="/images/figure.svg"
-      />
-      <Image
-        alt={'new-arrive-text'}
-        className="object-cover rounded-sm"
-        fill
-        loading="lazy"
-        sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
-        src="/images/new_arrive.svg"
-      />
+    <Flexbox gap={24} horizontal width={'100%'}>
+      <GridShowcase
+        innerProps={{ gap: 24 }}
+        style={{ maxHeight: 'calc(100% - 104px)', maxWidth: 1024, minHeight: 162 }}
+        width={'100%'}
+      >
+        <Grid maxItemWidth={280} rows={4}>
+          {items.map((item, index) => (
+            <Card key={item.product?.identifier ?? index} showCategory {...item} />
+          ))}
+        </Grid>
+      </GridShowcase>
+      {!mobile && (
+        <Flexbox align="end" flex={1} height={'100%'} style={{ position: 'relative' }}>
+          <Image
+            alt={'new-arrive-figure'}
+            className="object-cover rounded-sm"
+            fill
+            loading="lazy"
+            sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
+            src="/images/figure.svg"
+          />
+          <Image
+            alt={'new-arrive-text'}
+            className="object-cover rounded-sm"
+            fill
+            loading="lazy"
+            sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
+            src="/images/new_arrive.svg"
+          />
+        </Flexbox>
+      )}
     </Flexbox>
   );
 });
